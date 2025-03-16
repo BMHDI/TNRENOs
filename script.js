c25a39287cb25cac6eb1f13eb2d9c22b4b3ac6a7
Tarik <tarikboumehdi91@gmail.com>
2025-03-16 00:18:16 -0600

    uupdated the form phone id

--- script.js
+++ script.js
@@ -85,12 +85,12 @@ document.addEventListener("DOMContentLoaded", function () {
   
         // Get the form data
         const formData = new FormData(form);
-        const name = formData.get("name");
-        const email = formData.get("email");
-        const message = formData.get("message");
-        const phone = formData.get("phone");
+        // const name = formData.get("name");
+        // const email = formData.get("email");
+        // const message = formData.get("message");
+        // const phone = formData.get("phone");
         const button = document.getElementById('send-email-button');
-        console.log(name, email, message , phone);
+       
   
         // Show sending...
         button.textContent = 'Sending...';
